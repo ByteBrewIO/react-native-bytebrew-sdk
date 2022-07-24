fc27547401ab40e288651dc8e4450568a430ad9a
ByteBrewIO <80991005+ByteBrewIO@users.noreply.github.com>
2022-07-24 15:51:55 -0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  BytebrewSdkExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-bytebrew-sdk': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
